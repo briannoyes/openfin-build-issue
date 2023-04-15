11848fd1b20121867b2c2dc16303fb87804b0c03
Brian Noyes <brian.noyes@solliance.net>
2023-04-15 16:17:18 -0700

    repro of build issue

---
+++ openfin-build-issue/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { fin } from '@openfin/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'openfin-build-issue';
+}
